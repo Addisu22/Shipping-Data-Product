a281c1640d113f43c1d3e3d5e0787f9e82ac31cb
Addisu22 <liveaddisu@gmail.com>
2025-07-14 23:07:45 +0300

    Initialization : Configuration

---
+++ Script/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Automatically reads from .env
+
+api_id     = os.getenv("tg_api_id")
+api_hash   = os.getenv("tg_api_hash")
+session_name = os.getenv("session_name")
+host     = os.getenv("pgsql_host"),
+port     = os.getenv("pgsql_port"),
+db       = os.getenv("pgsql_db"),
+user     = os.getenv("pgsql_user"),
+password = os.getenv("pgsql_pass")
