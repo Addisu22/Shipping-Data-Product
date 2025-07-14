2a585531e3fad0b5f12c55fb92d991e700bf484f
Addisu22 <liveaddisu@gmail.com>
2025-07-14 23:42:43 +0300

    Initialization : Data Connection

--- Script/connection.py
+++ Script/connection.py
@@ -1,11 +1,11 @@
 import psycopg2
 from config import host, port, db, user, password
 
-def getpgsql_connect(host, port, db, user, password):
+def getpgsql_connect():
     conn = psycopg2.connect(
 							host = host,
 							port = port,
 							db   = db,
 							user = user,
-							password = password
-    )
+							password = password)
+    return conn
