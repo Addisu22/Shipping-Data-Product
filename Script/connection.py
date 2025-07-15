db7cd221adf43f4746ac77105b43c973244139a6
Addisu22 <liveaddisu@gmail.com>
2025-07-15 08:20:32 +0300

    Initialization : Data Connection

--- Script/connection.py
+++ Script/connection.py
@@ -6,9 +6,10 @@ from config import pgsql_host, pgsql_port, pgsql_db, pgsql_user, pgsql_password
 
 def getpgsql_connect():
     conn = psycopg2.connect(
-							host = pgsql_host,
-							port = pgsql_port,
-							dbname   = pgsql_db,
-							user = pgsql_password,
-							password = pgsql_password)
+							pgsql_host       = os.getenv("pgsql_host"),
+							pgsql_port       = int(os.getenv("pgsql_port")),
+							pgsql_db         = os.getenv("pgsql_db"),
+							pgsql_user       = os.getenv("pgsql_user"),
+							pgsql_password   = os.getenv("pgsql_pass")
+	)
     return conn
