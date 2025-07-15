e81cbddf2f4bb988fe4579837f25fd8c82c0af5e
Addisu22 <liveaddisu@gmail.com>
2025-07-15 15:49:24 +0300

    Build an Analytical API Fast API

--- Script/dbconnection.py
+++ Script/dbconnection.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Our actual database connection string
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Admin@123@localhost/telegram"
+db_url = "postgresql://postgres:Admin@123@localhost/telegram"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
