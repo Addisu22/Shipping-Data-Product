ff2c0faea7ea935f4d4e2d6b7fb26428ae51fdb3
Addisu22 <liveaddisu@gmail.com>
2025-07-15 08:28:39 +0300

    Initialization : Data Connection

--- Script/connection.py
+++ Script/connection.py
@@ -6,10 +6,10 @@ load_dotenv(dotenv_path="../.env")  # Automatically reads from .env
 
 def getpgsql_connect():
     conn = psycopg2.connect(
-							pgsql_host       = os.getenv("pgsql_host"),
-							pgsql_port       = int(os.getenv("pgsql_port")),
-							pgsql_db         = os.getenv("pgsql_db"),
-							pgsql_user       = os.getenv("pgsql_user"),
-							pgsql_password   = os.getenv("pgsql_pass")
-	)
+        host=os.getenv("pgsql_host"),
+        port=int(os.getenv("pgsql_port")),
+        dbname=os.getenv("pgsql_db"),
+        user=os.getenv("pgsql_user"),
+        password=os.getenv("pgsql_pass")
+    )
     return conn
